7628b1d0f0055afb0aec79c4609d541d4c1742e0
Paweł Maciejewski <fragphace@gmail.com>
2013-01-06 20:06:00 +0100

    Added version info

--- bin/basset.js
+++ bin/basset.js
@@ -1,23 +1,51 @@
 #!/usr/bin/env node
 
-var Basset, ReporterFactory, argv, basset, optimist, reporter;
-ReporterFactory = require('../lib/reporterFactory');
-Basset = require('../lib/basset');
-optimist = require('optimist').usage('basset [options] url').demand(1).boolean('h').alias('h', 'help').describe('h', 'This screen').string('n').alias('n', 'num').describe('n', 'Num of repeats')["default"]('n', '1').string('r').alias('r', 'reporter').describe('r', 'Reporter ' + '(' + Object.keys(ReporterFactory.reporters).join(', ') + ')')["default"]('r', 'minimal').check(function(args) {
-  if (args.n && (parseInt(args.n, 10).toString() !== args.n)) {
-    throw new Error('Num must be a decimal number');
-  }
-});
-argv = optimist.argv;
+var ReporterFactory = require('../lib/reporterFactory');
+var Basset = require('../lib/basset');
+var optimist = require('optimist')
+	.usage('basset [options] url')
+	.demand(1)
+
+	.boolean('h')
+	.alias('h', 'help')
+	.describe('h', 'This screen')
+
+	.boolean('v')
+	.alias('v', 'version')
+	.describe('v', 'Version info')
+
+	.string('n')
+	.alias('n', 'num')
+	.describe('n', 'Num of repeats')
+	.default('n', '1')
+
+	.string('r')
+	.alias('r', 'reporter')
+	.describe('r', 'Reporter ' + '(' + Object.keys(ReporterFactory.reporters).join(', ') + ')')
+	.default('r', 'minimal')
+
+	.check(function(args) {
+	  if (args.n && (parseInt(args.n, 10).toString() !== args.n)) {
+	    throw new Error('Num must be a decimal number');
+	  }
+	});
+
+var argv = optimist.argv;
 
 if (argv.h) {
   optimist.showHelp();
   process.exit();
 }
 
-basset = new Basset(argv._[0], {
+if (argv.v) {
+	package = require('../package.json')
+	console.log('v' + package.version)
+	process.exit();
+}
+
+var basset = new Basset(argv._[0], {
   repeatNum: argv.n
 });
-reporter = ReporterFactory.createReporter(argv.r);
+var reporter = ReporterFactory.createReporter(argv.r);
 reporter.bind(basset);
 basset.sniff();
